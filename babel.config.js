d8a9c3b4c699df7ee6348700fc4abe071d4041a4
ferreiradiniz <ferreiradiniz@msn.com>
2026-02-23 21:29:40 +0000

    add `database.js` and `.env`

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+// Source - https://stackoverflow.com/a/64223627
+// Posted by ajwl, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-19, License - CC BY-SA 4.0
+
+module.exports = { presets: ["@babel/preset-env"] };
