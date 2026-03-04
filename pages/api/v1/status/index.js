90e670e73ba66bb2453cdbb0339a4c00a1e5ade1
ferreiradiniz <ferreiradiniz@msn.com>
2026-03-04 19:17:24 +0000

    finish `/ap1/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1;");
-  response.status(200).json({ chave: "são acima da média" });
+  const updateAt = new Date().toISOString();
+
+  const dataBaseResult = await database.query("SHOW server_version;");
+  const databaseVersion = dataBaseResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnections = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const dataBaseOpenedConnections =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependences: {
+      database: {
+        version: databaseVersion,
+        max_connections: databaseMaxConnections,
+        opened_connections: dataBaseOpenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,30 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+let response;
+
+beforeAll(async () => {
+  response = await fetch("http://localhost:3000/api/v1/status");
+  responseBody = await response.json();
+});
+
+describe("GET to /api/v1/status", () => {
+  it("should return STATUS 200", async () => {
+    expect(response.status).toBe(200);
+  });
+
+  it("should return UPDATE AT", async () => {
+    expect(responseBody.update_at).toBeDefined();
+    const parseUpdateAt = new Date(responseBody.update_at).toISOString();
+    expect(responseBody.update_at).toEqual(parseUpdateAt);
+  });
+
+  it("should return VERSION DATA BASE", async () => {
+    expect(responseBody.dependences.database.version).toEqual("18.2");
+  });
+
+  it("should return MAX CONNECTIONS igual a 100", async () => {
+    expect(responseBody.dependences.database.max_connections).toEqual(100);
+  });
+
+  it("should return OPENED CONNECTIONS igual 1", async () => {
+    expect(responseBody.dependences.database.opened_connections).toEqual(1);
+  });
 });
