f9b363c099a559e2d33376913dd106166cdcc0ed
ferreiradiniz <ferreiradiniz@msn.com>
2026-03-09 21:35:54 +0000

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,5 @@
 import { Client } from "pg";
-// const Pool = require("pg-pool");
 
-// const config = {
-//   host: process.env.POSTGRES_HOST,
-//   port: process.env.POSTGRES_PORT,
-//   user: process.env.POSTGRES_USER,
-//   database: process.env.POSTGRES_DB,
-//   password: process.env.POSTGRES_PASSWORD,
-//   ssl: false,
-//   max: 20, // set pool max size to 20
-//   idleTimeoutMillis: 1000, // close idle clients after 1 second
-//   connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-//   maxUses: 7500,
-// };
-
-// const pool = new Pool(config);
 const postgresConfig = {
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
@@ -24,10 +9,16 @@ const postgresConfig = {
   ssl: getSSLValues(),
 };
 
-async function query(queryObject) {
+async function getNewClient() {
   const client = new Client(postgresConfig);
+  await client.connect();
+  return client;
+}
+
+async function query(queryObject) {
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -39,7 +30,8 @@ async function query(queryObject) {
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
@@ -48,5 +40,5 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
